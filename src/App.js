5946a42db1f934352aeba26ab8b6e5460f920ad3
Abhishek Madhu <21152008+abhishekmadhu@users.noreply.github.com>
2022-07-16 18:10:33 +0530

    Before starting monster-rolodex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor () {
+    super();
+
+    this.state = {
+      name: 'Abhishek'
+    };
+  }
+
+  // handleChange(){
+  //   this.setState({
+  //     name: 'China'
+  //   });
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi { this.state.name }
+          </p>
+          {/* <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+          <button onClick={() => {
+            this.setState(
+              (state, props) => {
+                return {
+                  name: state.name === 'Abhishek' ? 'China' : 'Abhishek'
+                };
+              },
+              () => {
+                console.log(this.state);
+              })
+            }}>
+            Change Name
+          </button>          
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
