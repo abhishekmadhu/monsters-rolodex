ff9fcb6bbffd7aecf2877165832278369976837a
Abhishek Madhu <21152008+abhishekmadhu@users.noreply.github.com>
2022-07-17 16:58:10 +0530

    End of chapter 39 - before I attempt myself

--- src/App.js
+++ src/App.js
@@ -1,54 +1,49 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 
-  constructor () {
+  constructor() {
+    console.log('constructor-begin');
     super();
 
     this.state = {
-      name: 'Abhishek'
+      monsters: []
     };
   }
 
-  // handleChange(){
-  //   this.setState({
-  //     name: 'China'
-  //   });
-  // }
+  // 
+  componentDidMount() {
+    console.log('componentDidMount-begin');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState(() => {
+          return { monsters: users }; 
+        },
+          () => {
+          console.log(this.state);
+        })
+      });
+  }
 
   render() {
+    console.log('render-begin');
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi { this.state.name }
-          </p>
-          {/* <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
-          <button onClick={() => {
-            this.setState(
-              (state, props) => {
-                return {
-                  name: state.name === 'Abhishek' ? 'China' : 'Abhishek'
-                };
-              },
-              () => {
-                console.log(this.state);
-              })
-            }}>
-            Change Name
-          </button>          
-        </header>
+        <input className='search-box' type='search' placeholder='search monsters' onChange={
+          (event) => { console.log(event.target.value); return null; }
+        } />
+        {
+          this.state.monsters.map(
+            (monster) => {
+              return <div key={monster.name}>
+                <h1>{monster.name}</h1>
+              </div>
+            })
+        }
       </div>
     );
   }
